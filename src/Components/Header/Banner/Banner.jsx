bfcb62f0ce3649ee82c1a93d3c6ca28709b736fc
surovishimu <surovi.shimu95@gmail.com>
2023-10-20 20:37:44 +0600

    change the banner height for each device

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -5,14 +5,14 @@ const Banner = () => {
       <img
         src="https://png.pngtree.com/thumb_back/fw800/back_our/20190621/ourmid/pngtree-high-end-atmosphere-simple-wind-makeup-area-banner-image_195398.jpg"
         alt=""
-        className="w-full h-[80vh]"
+        className="w-full lg:h-[80vh] md:h-[60vh] h-[50vh]"
       />
       <div className="absolute top-0 left-0 w-full h-full bg-opacity-50 bg-black flex flex-col justify-center items-center text-white p-4">
         <h1 className="lg:text-6xl md:text-4xl text-3xl font-semibold mb-4" data-aos="fade-left"
           data-aos-anchor="#example-anchor"
           data-aos-offset="500"
           data-aos-duration="500">Elegance in Bloom</h1>
-        <p className="lg:text-xl md:text-lg text-sm" data-aos="fade-right"
+        <p className="lg:text-xl md:text-lg text-sm text-center" data-aos="fade-right"
           data-aos-anchor="#example-anchor"
           data-aos-offset="500"
           data-aos-duration="500">Discover the Beauty of Orchids - Your Gateway to Radiant Elegance</p>
--- src/Components/Header/Navbar/Logo.jsx
+++ src/Components/Header/Navbar/Logo.jsx
@@ -3,7 +3,7 @@
 const Logo = () => {
     return (
         <div className="">
-            <span className="flex justify-around items-center text-2xl font-semibold text-amber-800"> <img className="w-24 h-16" src="https://i.ibb.co/QbZNnqm/pngtree-skincare-logo-png-image-6325549.png" alt="" />Orchid Beauty</span>
+            <span className="flex justify-around items-center lg:text-2xl md:text-2xl text-xl font-semibold text-amber-800"> <img className="w-24 h-16" src="https://i.ibb.co/QbZNnqm/pngtree-skincare-logo-png-image-6325549.png" alt="" />Orchid Beauty</span>
 
         </div>
     );
